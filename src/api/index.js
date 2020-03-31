29940e48b37c1f24270b6be814aa549997277c9f
jorgesabella <jorge_andres1996@hotmail.com>
2020-03-30 20:49:23 -0600

    Added tag endpoint.

--- src/api/index.js
+++ src/api/index.js
@@ -3,6 +3,7 @@ import user from './user'
 import auth from './auth'
 import passwordReset from './password-reset'
 import item from './item'
+import tag from './tag'
 
 const router = new Router()
 
@@ -33,5 +34,6 @@ router.use('/users', user)
 router.use('/auth', auth)
 router.use('/password-resets', passwordReset)
 router.use('/items', item)
+router.use('/tags', tag)
 
 export default router
